9c702edfff548d509ac346d261b038854f2f40d8
Gesho05 <131277958+Gesho05@users.noreply.github.com>
2025-12-01 01:19:03 +0200

    reworked the homepage buttons

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,12 +5,16 @@ defineProps({
     bg:{
         type: String,
         default: 'bg-gray-100'
+    },
+    hover:{
+        type: String,
+        default: 'hover:bg-gray-200 transition-colors duration-300'
     }
 });
 </script>
 
 <template>
-    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <div :class="`${bg} ${hover} p-6 rounded-lg shadow-md text-center cursor-pointer`">
         <slot></slot>
     </div>
 </template>
--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -4,33 +4,25 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-     <section class="py-4">
+     <section class="py-4 mx-40">
       <div class="container-xl lg:container m-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-16 p-4 rounded-lg">
+           <RouterLink to="/jobs">
           <Card>
             <h2 class="text-2xl font-bold">For Developers</h2>
-            <p class="mt-2 mb-4">
+            <p class="mt-2 mb-2">
               Browse our Vue jobs and start your career today
             </p>
-            <RouterLink
-              to="/jobs"
-              class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Jobs
-            </RouterLink>
           </Card>
-          <Card bg="bg-green-100">
+        </RouterLink>
+          <RouterLink to="/jobs/add">
+          <Card bg="bg-green-100" hover="hover:bg-green-200 transition-colors duration-300">
             <h2 class="text-2xl font-bold">For Employers</h2>
-            <p class="mt-2 mb-4">
+            <p class="mt-2 mb-2">
               List your job to find the perfect developer for the role
             </p>
-            <RouterLink
-              to="/jobs/add"
-              class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
-            >
-              Add Job
-            </RouterLink>
           </Card>
+        </RouterLink>
         </div>
       </div>
     </section>
